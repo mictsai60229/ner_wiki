ccc15edd9bf721a2dade2d25c5f171d68de223f5
mictsai60229 <mictsai@nlplab.cc>
2020-07-12 12:41:56 +0000

    refactor: remove sqlite3 command

--- collect_data/download_data.sh
+++ collect_data/download_data.sh
@@ -7,5 +7,5 @@ gunzip enwiki-latest-langlinks.sql.gz
 gunzip enwiki-latest-redirect.sql.gz
 
 
-./mysql2sqlite enwiki-latest-langlinks.sql | (cat && echo "select * from langlinks;") | sqlite3 -header -csv -separator $'\t' | sed -e '1,2d' > enwiki-latest-langlinks.tsv
-./mysql2sqlite enwiki-latest-redirect.sql | (cat && echo "select * from redirect;") | sqlite3 -header -csv -separator $'\t' | sed -e '1,2d' > enwiki-latest-redirect.tsv
+./mysql2sqlite enwiki-latest-langlinks.sql > enwiki-latest-langlinks.sqlite3
+./mysql2sqlite enwiki-latest-redirect.sql > enwiki-latest-redirect.sqlite3
